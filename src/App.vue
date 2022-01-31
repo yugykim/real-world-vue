6c16df5bb53f2a0df86eb151be69e77e00f79a6c
yugykim <82011274+yugykim@users.noreply.github.com>
2022-01-30 21:28:12 -0700

    to do list with Vue.js,

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <div id="nav">
+      <router-link :to="{ name: 'EventList' }">Events</router-link> |
+      <router-link :to="{ name: 'About' }">About</router-link>
+    </div>
+    <router-view />
   </div>
-  <router-view />
 </template>
 
 <style>
@@ -26,5 +28,9 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+h4 {
+  font-size: 20px;
 }
 </style>
