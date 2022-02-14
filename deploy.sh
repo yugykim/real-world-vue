79194668cc5a54b79ecc9ac0c9490ee022771264
yugykim <82011274+yugykim@users.noreply.github.com>
2022-02-14 12:27:00 -0700

    test

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,6 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:yugykim/real-world-vue.git master:gh-pages
+git push -f git@github.com/yugykim/real-world-vue.git master:gh-pages
 cd -
+
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/real-world-vue/'
+  ? '/real-vue/real-world-vue/'
   : '/'
 }
