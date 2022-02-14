f8e12563c7fcf91433034e54ade58a168023cef3
yugykim <yugyeongk92@gmail.com>
2022-02-14 15:28:46 -0700

    test

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env sh
 # abort on errors
+
 set -e
+
 # build
-yarn run build
+
+npm run build
+
 # navigate into the build output directory
+
 cd dist
+
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
+
 git init
+
 git add -A
+
 git commit -m 'deploy'
+
 git push -f git@github.com/yugykim/real-world-vue.git master:gh-pages
+
 cd -
 
